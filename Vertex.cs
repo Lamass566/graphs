8643f981239e1e2c6d26978469e2656738d2e2e0
Миша <vsisoev@ukr.net>
2022-01-13 15:15:47 +0200

    Add project files.

---
+++ Vertex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphs
+{
+    class Vertex
+    {
+        public int Number { get; set; }
+        public bool IsVisited;
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+        public Vertex(int number)
+        {
+            Number = number;
+        }
+    }
+}
